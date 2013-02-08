4ffd97898b8b52a8cdaf1ae55e85facd5e4855d5
Patric M DelCioppo <pdelcioppo@gmail.com>
2013-02-08 13:55:51 -0500

    moved docs to readme

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 slingshot
 =========
 
-Simple wrapper to expose Pyslinger as a service
+Slingshot is a simple wrapper to expose [Pyslinger](https://github.com/sevennineteen/pyslinger) as a service.
+
+This provides a simple HTTP bridge to the CQ loading service, allowing the rest of the migration project to be developed using whatever you like. 
+
+The only requirement is that you write your payloads to files in the Pyslinger-specified JSON format.
+
+## Usage
+
+Send a payload to Slingshot with an HTTP POST like so:
+
+    curl -F payload=@page_example.json http://localhost:5000/</code>
+
+Use `-F` to set additional configurations:
+
+
+* `cq_server` (defaults to **http://localhost:4502**)
+* `username` (defaults to **admin**)
+* `password` (defaults to **admin**)
+* `static_root` (defaults to **.**)
+
+For example:
+
+    curl -F payload=@asset_example.json -F username=me -F password=P@55w0rd -F static_root=/my/assets http://localhost:5000/
--- app.py
+++ app.py
@@ -1,32 +1,15 @@
 from flask import Flask, request, jsonify, Markup
 from pyslinger import pyslinger
 import simplejson as json
+from markdown import markdown
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def load_item():
     if request.method == 'GET':
-        return Markup('''
-            <h1>Slingshot</h1>
-            <pre>
-            Send a JSON payload to this URL like so:
-
-            curl -F payload=@page_example.json %s</code>
-
-            Use -F to set additional configurations:
-
-            * cq_server       [ http://localhost:4502 ]
-            * username        [ admin ]
-            * password        [ admin ]
-            * static_root     [ . ]
-
-            For example:
-
-            curl -F payload=@asset_example.json -F username=me -F password=P@55w0rd -F static_root=/my/assets http://localhost:5000/
-            </pre>
-
-            ''' % request.url)
+        # show usage docs
+        return Markup(markdown(open('README.md').read()))
     if request.method == 'POST' and 'payload' in request.files:
 
         # Update any pyslinger constants passed
