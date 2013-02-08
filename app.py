b5ce2676af8bd26980c1ed5c3f2a486b17134a9a
Patric M DelCioppo <pdelcioppo@gmail.com>
2013-02-08 13:21:32 -0500

    configuration notes

--- app.py
+++ app.py
@@ -9,12 +9,28 @@ def load_item():
     if request.method == 'GET':
         return Markup('''
             <h1>Slingshot</h1>
-            Send a JSON payload to this URL like so:<br> 
-            <code>curl -F payload=@page_example.json %s</code>
+            <pre>
+            Send a JSON payload to this URL like so:
+
+            curl -F payload=@page_example.json %s</code>
+
+            Use -F to set additional configurations:
+
+            * cq_server       [ http://localhost:4502 ]
+            * username        [ admin ]
+            * password        [ admin ]
+            * static_root     [ . ]
+
+            For example:
+
+            curl -F payload=@asset_example.json -F username=me -F password=P@55w0rd -F static_root=/my/assets http://localhost:5000/
+            </pre>
+
             ''' % request.url)
     if request.method == 'POST' and 'payload' in request.files:
 
-        # Update CQ credentials (cq_server, username, password) if passed
+        # Update any pyslinger constants passed
+        # cq_server, username, password, static_root
         for field in request.form:
             setattr(pyslinger, field.upper(), request.form[field])
 
